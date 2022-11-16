3f482973f645685d36eb16acac368f8350e12d77
Ivan Haurylenka <hip@uvr.aero>
2022-11-16 14:58:14 +0300

    add tracking with id

--- object_tracking.py
+++ object_tracking.py
@@ -10,55 +10,91 @@ import math
 def main():
 
 
-    object_detect = ObjectDetection()
+    od = ObjectDetection()
 
     cap = cv2.VideoCapture("los_angeles.mp4")
 
+    # Initialize count
     count = 0
+    center_points_prev_frame = []
 
-    center_point_dict = dict()
-    # center_points_list = list()
-    center_point_prv_frame = list()
-
-    tracking_objects = dict()
+    tracking_objects = {}
     track_id = 0
 
     while True:
     
         ret, frame = cap.read()
-
         count += 1
-
         if not ret:
             break
 
-        center_point_current_frame = list()
+        # Point current frame
+        center_points_cur_frame = []
 
-
-        (class_ids, scores, boxes) = object_detect.detect(frame=frame)
-
+        # Detect objects on frame
+        (class_ids, scores, boxes) = od.detect(frame)
         for box in boxes:
             (x, y, w, h) = box
-            central_point_x = int((x + x + w) / 2)
-            central_point_y = int((y + y + h) / 2)
-            center_point_current_frame.append((central_point_x, central_point_y))
+            cx = int((x + x + w) / 2)
+            cy = int((y + y + h) / 2)
+            center_points_cur_frame.append((cx, cy))
+            #print("FRAME N°", count, " ", x, y, w, h)
+
+            # cv2.circle(frame, (cx, cy), 5, (0, 0, 255), -1)
             cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
 
-        for point in center_point_current_frame:
-            for point2 in center_point_prv_frame:
-                distance = math.hypot(point2[0] - point[0], point2[1] - point[1])
+        # Only at the beginning we compare previous and current frame
+        if count <= 2:
+            for pt in center_points_cur_frame:
+                for pt2 in center_points_prev_frame:
+                    distance = math.hypot(pt2[0] - pt[0], pt2[1] - pt[1])
 
-                if distance < 10:
-                    tracking_objects[track_id] = point
-                    track_id += 1
+                    if distance < 20:
+                        tracking_objects[track_id] = pt
+                        track_id += 1
+        else:
+
+            tracking_objects_copy = tracking_objects.copy()
+            center_points_cur_frame_copy = center_points_cur_frame.copy()
+
+            for object_id, pt2 in tracking_objects_copy.items():
+                object_exists = False
+                for pt in center_points_cur_frame_copy:
+                    distance = math.hypot(pt2[0] - pt[0], pt2[1] - pt[1])
+
+                    # Update IDs position
+                    if distance < 20:
+                        tracking_objects[object_id] = pt
+                        object_exists = True
+                        if pt in center_points_cur_frame:
+                            center_points_cur_frame.remove(pt)
+                        continue
+
+                # Remove IDs lost
+                if not object_exists:
+                    tracking_objects.pop(object_id)
+
+            # Add new IDs found
+            for pt in center_points_cur_frame:
+                tracking_objects[track_id] = pt
+                track_id += 1
 
         for object_id, pt in tracking_objects.items():
             cv2.circle(frame, pt, 5, (0, 0, 255), -1)
             cv2.putText(frame, str(object_id), (pt[0], pt[1] - 7), 0, 1, (0, 0, 255), 2)
 
+        print("Tracking objects")
+        print(tracking_objects)
+
+
+        print("CUR FRAME LEFT PTS")
+        print(center_points_cur_frame)
+
+
         cv2.imshow("Frame", frame)
 
-        center_point_prv_frame = center_point_current_frame.copy()
+        # Make a copy of the points
+        center_points_prev_frame = center_points_cur_frame.copy()
 
         if cv2.waitKey(1) & 0xFF == ord('q'):
             break
